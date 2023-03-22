6755ef4f4ea3b77c64491d4ad539b27ed085a9a2
sehno <orion.sehn@gmail.com>
2023-03-22 11:21:49 -0600

    Added burnout analysis jupyter notebook, outlines work

--- split_data.py
+++ split_data.py
@@ -7,25 +7,6 @@ from sklearn.model_selection import StratifiedShuffleSplit
 data = pd.read_csv(r".\Data Carrard et al. 2022 MedTeach.csv")
 print(data)
 
-#plot histogram of data, mbi_ex
-plt.hist(data['mbi_ex'], bins=100, color='blue', edgecolor='black', alpha=0.65)
-plt.xlabel('mbi_ex')
-plt.ylabel('Frequency')
-# plt.show()
-# print(data['mbi_ex'].describe())
-
-plt.hist(data['mbi_cy'], bins=100, color='blue', edgecolor='black', alpha=0.65)
-plt.xlabel('mbi_cy')
-plt.ylabel('Frequency')
-# plt.show()
-# print(data['mbi_cy'].describe())
-
-plt.hist(data['mbi_ea'], bins=100, color='blue', edgecolor='black', alpha=0.65)
-plt.xlabel('mbi_ea')
-plt.ylabel('Frequency')
-# plt.show()
-# print(data['mbi_ea'].describe())
-
 
 #split half of the data randomly into training and validation sets
 split = StratifiedShuffleSplit(n_splits=1, test_size=0.5, random_state=42)
